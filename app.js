d9d51a69cc6dfdb68d70a6999eb3ce379ed42ea0
Ammar-Man <almandaa@arcada.fi>
2023-01-30 20:52:50 +0200

    vueJS user generator mini project

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ const app = Vue.createApp({
             lastName:"Code",
             email:"AmppariCode@google.com",
             gender:"male",
-            img:"https://picsum.photos/200/300"
+            country:"Norway",
+            img:"https://picsum.photos/200"
+        }
+    },
+    methods:{
+       async getUser(){
+            const res = await fetch("https://randomuser.me/api")
+            const {results } = await res.json()
+            console.log(results)
+            this.firstName = results[0].name.first,
+            this.lastName =results[0].name.last,
+            this.email =results[0].email,
+            this.gender =results[0].gender,
+            this.country = results[0].location.country
+            this.img = results[0].picture.large
         }
     }
 })
